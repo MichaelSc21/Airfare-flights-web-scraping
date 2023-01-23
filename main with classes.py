7ea4780b8b76f8d23d8403743fa4751cbec55991
MichaelSc21 <michaelscutariu31@gmail.com>
2023-01-23 21:27:41 +0000

    a few more changes

--- main with classes.py
+++ main with classes.py
@@ -1,26 +1,50 @@
 # %%
 import requests 
 import json
-from API_details import ACCESS_TOKEN, API_SECRET, API_KEY
-
+import API_details
+from importlib import reload
+reload(API_details)
+print(API_details.ACCESS_TOKEN)
 def get_data(origin, destination,departure, adults, children):
     url = f"https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode={origin}&destinationLocationCode={destination}&departureDate={departure}&adults={adults}&children={children}&nonStop=false&max=250"
 
     #payload='origin=BHX&destination=IAS&departureDate=2023-05-02&adults=1&nonStop=False'
+    payload = {}
     headers = {
-  'Authorization': f'Bearer {ACCESS_TOKEN}',
+  'Authorization': f'Bearer {API_details.ACCESS_TOKEN}',
   'Content-Type': 'application/x-www-form-urlencoded'
     }
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    print(response.text)
-
 
     data = json.loads(response.text)
 
-    with open('airfare#1.json', 'a') as f:
-        json.dump(data, f)
+    
+    print(data)
+    print(type(data))
+    with open('airfare#1.json', 'w+') as f:
+        
+        try:
+            data = data['data']
+            file_json = json.load(f)
+            try:
+                file_json[departure].append(data)
+                print('appended new flights to existing date')
+            except Exception as err:
+                print('adding flights to a new date')
+                print(err)
+                file_json[departure] = data
+        except Exception as e:
+            print(e)
+            print('creating a file in the first place')
+            del data[0]
+            file_json = {departure: data}
+        json.dump(file_json, f, indent = 2)
+    return data
 
-
-get_data('BHX', 'IAS', '2023-05-02', '2', '2')
+data = get_data('BHX', 'IAS', '2023-05-03', '4', '0')
+# %%
+# %%
+print(data)
+# %%
