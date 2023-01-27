779cc242f6235c3cbdedf2ffa86a3ce7c2c8fd7b
MichaelSc21 <michaelscutariu31@gmail.com>
2023-01-27 22:18:46 +0000

    Changed the structure of the functions so that I was able to use decorators

--- data analyser.py
+++ data analyser.py
@@ -5,65 +5,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 # %%
+def filename_getter(dict_locations) : # --> list
+    list_filenames = []
+    for origin in dict_locations['listOrigin']:
+            for destination in dict_locations['listDestination']:
+                filename = f'{origin}_to_{destination}.json'
+                list_filenames.append(filename)
+    return list_filenames
+
+
+def file_rotator(list_filenames, list_dfs =[]):
+    def wrapper(func):
+
+        def creating_dfs(*args, **kwargs):
+            for filename in list_filenames:
+                data = func(filename)
+                df = pd.DataFrame.from_dict(data, orient='index')
+                df.index = pd.to_datetime(df.index)
+                dict_dfs[filename[:-5]] = df
+                df['price'] = df.apply(lambda row: np.min(row[2]), axis = 1)
+                df['currency'] =  df['currency'].apply(lambda row: function1(row))
+                list_dfs.append(df)
+        return creating_dfs()
+    return wrapper
+
 def sanitisation(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
+        print(data)
     sanitised_data = {}
 
     for date in data:
-        sanitised_data[date]={
-                'no_trips':[],
-                'currency':[],
-                'price':[]
-         }
+
         for i in range(len(date)):
-
-
-            
             sanitised_data[date]['no_trips'].append(len(data[date][i]["itineraries"][0]['segments']))
             sanitised_data[date]['currency'].append(data[date][i]['price']['currency'])
 
             sanitised_data[date]['price'].append(data[date][i]['price']['total'])
 
-        
         sanitised_data[date]['price'] = np.array(sanitised_data[date]['price'], dtype = np.float32)
         
 
         print(sanitised_data[date]['price'])
-        """sanitised_data = {
-                'date': data[date][i]["itineraries"][0]['segments'][0]['departure']['at'],
-                'no_trips': len(data[date][i]["itineraries"][0]['segments']),
-                'curreny': data[date][i]['price']['currency'],
-                'price': data[date][i]['price']['total'],
-
-        }"""
     return sanitised_data
+
+
 
 def function1(row):
 
     return row[0]
 
 
-# %%
-dict_dfs={}
+
+# %% 
 if __name__ == '__main__':
-    listOrigin = ['BHX', 'MAN']
-    listDestination = ['IAS']
-    dict_dfs={}
-    for origin in listOrigin:
-        for destination in listDestination:
-            filename = f'{origin}_to_{destination}.json'
-            data = sanitisation(filename)
-            df = pd.DataFrame.from_dict(data, orient='index')
-            df.index = pd.to_datetime(df.index)
-            dict_dfs[filename[:-5]] = df
-            df['price'] = df.apply(lambda row: np.min(row[2]), axis = 1)
-            df['currency'] =  df['currency'].apply(lambda row: function1(row))
-# %%
-print(dict_dfs)
-fix, ax = plt.subplots(figsize= (12, 6))
-for df in dict_dfs.values():
-    print(df)
-    ax.plot(df.index, df['price'])
+    dict_locations={
+    'listOrigin':['BHX', 'MAN'],
+    'listDestination': ['IAS']
+}
+    list_filenames = filename_getter(dict_locations)
+    sanitisation = file_rotator(list_filenames)(sanitisation)
+    sanitisation()
+
 
