795627c05cb95ce201786aa88bb0058943d213d2
MichaelSc21 <michaelscutariu31@gmail.com>
2023-02-10 22:58:32 +0000

    Update main2.0.py

--- main2.0.py
+++ main2.0.py
@@ -44,6 +44,17 @@ def get_data(origin, destination,departure, adults, children):
 		print(data['errors'][0]['title'])
 		return None, departure
 
+def write_data_2(filename, month_dict):
+    try: 
+        with open(filename, 'r') as f:
+            file_json = json.load(f)
+            file_json = file_json | month_dict
+    except Exception as e:
+        print(e) 
+        file_json = month_dict
+    with open(filename, 'w') as f:
+        json.dump(file_json, f, indent=2, sort_keys=True)
+
 def write_data(filename, data, departure):
 	# this is meant to add the data for each individual day to the file
 	# this is a task which I/O bound I believe
@@ -95,16 +106,18 @@ def write_data_in_chunks(filename, data):
 # By default, period will be greater than the number of days in any month because the function will automatically result in using the length of the month as the period
 
 
-def rotate_date(future = None, origin=None, destination=None, month=None, dayResume = 1, period=40):
+def rotate_date(origin=None, destination=None, month=None, dayResume = 1, period=40):
+	month_dict = {}
 	months_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 	filename = f'{origin}_to_{destination}.json'
 	if month< 10:
 		string_month = '0'+str(month)
 	else:
 		string_month = month
-	month_dict = {}
+
 	dayEnd = dayResume + period
 	if dayEnd > months_list[month-1]+1:
+		#print('We are using the period, not the limit of the month')
 		dayEnd =months_list[month-1] +1
 
 	for day in range(dayResume, dayEnd):
@@ -113,21 +126,17 @@ def rotate_date(future = None, origin=None, destination=None, month=None, dayRes
 		else:
 			string_day = day
 
-		print(f"The date is {string_day}-{string_month}-2023")
-		print(f'THis is for {filename}')
-		print(''']
-		
-		
-		
-		''')
+		#print(f"The date is {string_day}-{string_month}-2023")
+		#print(f'THis is for {filename}')
+
 		data, departure = get_data(origin, destination, f'2023-{string_month}-{string_day}', '4', '0')
-		print(type(data))
+		#print(type(data))
 		#write_data(filename, data, departure)
 		#sanitised_data =data_analyser.sanitisation_2(data=data)
+		# Use this sanitised_data with write_data_2 rather than anything else
 		month_dict[departure] = data
-		print(sys.getsizeof(month_dict))
+		#print(sys.getsizeof(month_dict))
 		
-	print(day)
 	return month_dict, filename, day+1
 			
 
@@ -136,40 +145,40 @@ def rotate_date(future = None, origin=None, destination=None, month=None, dayRes
 # %%
 if __name__ == '__main__':
 	get_token()
-	months = [4, 5]
+	months = [3, 4, 5, 6, 7, 8, 9]
 	dict_locations = {
-		'listOrigin':['BHX', 'MAN'],
+		'listOrigin':['BHX'],
 		'listDestination': ['IAS']
 	}
 	
 
 	for origin in dict_locations['listOrigin']:
 		for destination in dict_locations['listDestination']:
+			period =11
 
+			with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+				dayResume = 1
+				
+				for periodic in range(3):
+					print(f"The dayResume is: {dayResume}")
+					print("""
+				
+				
+					""")
+					worker_list = []
+					for month in months:
+						worker_list.append(executor.submit(rotate_date, origin=origin, destination=destination, month=month,dayResume=dayResume, period = period))
+						time.sleep(0.5)
 
-			with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-				worker_list = []
-				for month in months:
-					worker_list.append(executor.submit(rotate_date, origin=origin, destination=destination, month=month, period = 5))
-					time.sleep(0.5)
-
+				
+					for future in concurrent.futures.as_completed(worker_list):
+						# this is for days 1-11
+						month_dict, filename, dayResume = future.result()
+						write_data_in_chunks(filename=filename, data=month_dict)
 			
-			for future in concurrent.futures.as_completed(worker_list):
-				print(future.result())
-				# this is for days 1-11
-				month_dict, filename, dayResume = future.result()
-				write_data_in_chunks(filename=filename, data=month_dict)
-				future.submit(rotate_date, origin = origin, destination = destination, month = month, dayResume = dayResume, period = 6)
-
-				"""# for days 12-23
-				future.add_done_callback(rotate_date(origin = origin, destination = destination, month = month, dayResume = dayResume))
-				write_data(filename=filename, month_dict=month_dict)
-				# for days 24- to end of month
-				write_data(filename=filename, month_dict=month_dict)
-				future.add_done_callback(rotate_date(origin = origin, destination = destination, month = month, dayResume = dayResume))
-				"""
 
 
 
+## %%
 
 # %%
